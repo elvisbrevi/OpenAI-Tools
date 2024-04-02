5938e9f79a8e150ba0e9cf4ebdae6b641e5e25cd
Elvis Brevi <elvisbrevi@gmail.com>
2024-04-02 10:10:29 -0300

    feat: spliting logic in files and adding tabs for access to different tools

---
+++ pages/video_to_text.py
@@ -0,0 +1,31 @@
+from pytube import YouTube
+import streamlit as st
+
+def download_mp3_from_youtube(url):
+    yt = YouTube(url)
+    stream = yt.streams.filter(only_audio=True).first()
+    stream.download(filename='audio.mp3')
+    
+def speech_to_text(openai_client, file):
+    client = openai_client#OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+    audio_file= open(file, "rb")
+    transcription = client.audio.transcriptions.create(
+        model="whisper-1", 
+        file=audio_file
+    )
+    return transcription.text
+
+def render(openai_client):
+    st.header('Read Youtube Video', divider='rainbow')
+    # Input to typing OpenAI Key
+    openai_key = st.text_input("OpenAI Key:", "")
+    # Input to typing Youtube URL
+    youtube_url = st.text_input("Youtube URL:", "")
+    # Output language
+    option = st.selectbox(
+        'Output Language:',
+        ('English', 'Spanish', 'French'))
+    # Buton to download mp3 from video
+    if st.button("Read Video!"):
+        download_mp3_from_youtube(youtube_url)
+        st.write(speech_to_text(openai_client, "audio.mp3"))
--- script.py
+++ script.py
@@ -1,36 +1,10 @@
-from pytube import YouTube
-from openai import OpenAI
 import streamlit as st
+from openai import OpenAI
+from pages import video_to_text
 
-# function for download youtube to mp3 
-def download_mp3_from_youtube(url):
-    yt = YouTube(url)
-    stream = yt.streams.filter(only_audio=True).first()
-    stream.download(filename='audio.mp3')
+openai_client = OpenAI()
 
-def speech_to_text(speech):
-    client = OpenAI()
-    audio_file= open(speech, "rb")
-    transcription = client.audio.transcriptions.create(
-        model="whisper-1", 
-        file=audio_file
-    )
-    return transcription.text
+tabAnalyzeContent, tabCreateImage = st.tabs(["Analyze Content", "Create Image"])
 
-st.header('Read Youtube Video', divider='rainbow')
-
-# Input to typing OpenAI Key
-openai_key = st.text_input("OpenAI Key:", "")
-
-# Input to typing Youtube URL
-youtube_url = st.text_input("Youtube URL:", "")
-
-# Output language
-option = st.selectbox(
-    'Output Language:',
-    ('English', 'Spanish', 'French'))
-
-# Buton to download mp3 from video
-if st.button("Read Video!"):
-    download_mp3_from_youtube(youtube_url)
-    st.write(speech_to_text("audio.mp3"))
+with tabAnalyzeContent:
+    video_to_text.render(openai_client)
