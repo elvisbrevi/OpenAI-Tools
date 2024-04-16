9e0233454178fa6f13cc24cb853f13d6f6a08128
Elvis Brevi <elvisbrevi@gmail.com>
2024-04-16 06:47:49 -0400

    set openai input value with default openai key from env

--- script.py
+++ script.py
@@ -4,9 +4,9 @@ from openai import OpenAI
 from pages import video_to_text
 
 # Input to typing OpenAI Key
-openai_key = st.text_input("OpenAI Key:")
+openai_key = st.text_input("OpenAI Key:", os.environ.get("OPENAI_API_KEY"))
 openai_client = OpenAI(
-    api_key = openai_key or os.environ.get("OPENAI_API_KEY")
+    api_key = openai_key
 )
 
 tabAnalyzeContent, tabCreateImage = st.tabs(["Analyze Content", "Create Image"])
